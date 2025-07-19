30a4aba73d6f9ba40d94a130aa0b55a23ad53579
FrogOfJuly <kirill.golubev@utu.fi>
2025-07-19 18:46:55 +0300

    unavailable elements should be invisiable

--- player.gd
+++ player.gd
@@ -3,3 +3,9 @@ extends Node
 
 @export var selectedElement = GLOBALS.Element_type.WATER;
 @export var mana = 100
+@export var availabeElements = {
+	GLOBALS.Element_type.WATER : null,
+	GLOBALS.Element_type.EARTH : null,
+	GLOBALS.Element_type.AIR   : null,
+	GLOBALS.Element_type.FIRE  : null,
+}
